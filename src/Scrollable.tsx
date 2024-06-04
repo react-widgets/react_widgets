0fec253bb77094286e56f37717e39752e3024ece
MTtankkeo <ttankkeo112@gmail.com>
2024-06-04 19:21:05 +0900

    Add vertical direction about Scrollable

--- src/Scrollable.tsx
+++ src/Scrollable.tsx
@@ -18,4 +18,20 @@ export namespace Scrollable {
             </div>
         )
     }
+
+    export function Vertical(p: {
+        children: ReactNode,
+    }) {
+        const style: CSSProperties = {
+            display: "flex",
+            overflowY: "auto",
+            height: "100%"
+        }
+
+        return (
+            <div style={style}>
+                <div style={{width: "100%", height: "100%", maxHeight: "max-content"}}>{p.children}</div>    
+            </div>
+        )
+    }
 }
