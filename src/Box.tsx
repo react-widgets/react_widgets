16cf74c615ca82afb22e372d22090958a3f5605d
MTtankkeo <ttankkeo112@gmail.com>
2024-06-08 17:35:42 +0900

    Add tabIndex in Box

--- src/Box.tsx
+++ src/Box.tsx
@@ -4,10 +4,19 @@ export interface BoxProperties extends CSSProperties {
     refer?: Ref<HTMLDivElement>,
     className?: string,
     children?: ReactNode,
+    tappable?: boolean,
 
     [key: string]: any;
 }
 
 export function Box(p: BoxProperties) {
-    return <div ref={p.refer} className={p.className} style={p}>{p.children}</div>
+    return (
+        <div
+            ref={p.refer}
+            className={p.className}
+            tabIndex={p.tappable ? 0 : null}
+            style={p}
+            children={p.children}
+        />
+    )
 }
