1ce83b023987e6ab9a405f0654f6f1c2dfb8e93f
MTtankkeo <ttankkeo112@gmail.com>
2024-06-16 01:19:26 +0900

    Add AnimatedPage.tsx

--- src/AnimatedPage.tsx
+++ src/AnimatedPage.tsx
@@ -1,6 +1,7 @@
-import { ReactNode, useLayoutEffect, useReducer, useState } from "react"
+import { ReactNode, Ref, useLayoutEffect, useReducer, useRef, useState } from "react"
 import { AnimatedSize } from "./AnimatedSize";
 import { Row } from "./Row";
+import { Box } from "./Box";
 
 export type AnimatedPageListener = (event: AnimatedPageEvent) => void;
 
@@ -35,7 +36,7 @@ export class AnimatedPageController {
     }
 
     removeListener(callback: AnimatedPageListener) {
-        console.assert(!this._listeners.includes(callback), "Already not registered a given listener.")
+        console.assert(this._listeners.includes(callback), "Already not registered a given listener.")
         this._listeners = this._listeners.filter(l => l != callback);
     }
     
@@ -44,42 +45,94 @@ export class AnimatedPageController {
     }
 }
 
-export function AnimatedPage({children, controller, behavior}: {
+export class AnimatedPageElement extends HTMLElement {
+    connectedCallback() {
+
+    }
+}
+
+export enum AnimatedPageStatus {
+    none,
+    push,
+    pop,
+}
+
+export function AnimatedPage({children, controller, pushBehavior, popBehavior}: {
     children: ReactNode,
     controller: AnimatedPageController,
-    behavior: AnimatedPageBehavior,
+    pushBehavior: AnimatedPageBehavior,
+    popBehavior: AnimatedPageBehavior,
 }) {
-    const reducer = (states: ReactNode[], {type, target}: { type: string; target?: ReactNode }) => {
-        if (type == "push") {
-            return [...states, target];
-        } else {
-            if (states.length == 1) {
-                throw new Error("Can't more pages to pop because pages is one.");
-            } else {
-                return states.splice(0, states.length - 1);
-            }
-        }
-    };
-    const [pages, setPages] = useReducer(reducer, [children]);
+    const cpRef = useRef<HTMLDivElement>(null); // is current page.
+    const rpRef = useRef<HTMLDivElement>(null); // is reference page.
+    const [pages, setPages] = useState<ReactNode[]>([children]);
+    const [status, setStatus] = useState(AnimatedPageStatus.none);
 
     useLayoutEffect(() => { // init state.
         let listener: AnimatedPageListener;
         controller.addListeners(listener = event => {
             if (event.type == "push") {
-                return setPages({type: "push", target: event.target});
+                return setStatus(AnimatedPageStatus.push), setPages([...pages, event.target]);
             }
             
             if (event.type == "pop") {
-                return setPages({type: "pop"});
+                return setStatus(AnimatedPageStatus.pop);
             }
         });
         
-        return controller.removeListener(listener);
+        return () => controller.removeListener(listener);
     }, []);
 
+    useLayoutEffect(() => {
+        const cPage = cpRef.current;
+        const rPage = rpRef.current?.firstElementChild as HTMLElement;
+
+        if (status == AnimatedPageStatus.none) return;
+        if (status == AnimatedPageStatus.push) {
+            const behavior = pushBehavior;
+            cPage.style.animation = `${behavior.fadeInKeyframeName} ${behavior.duration}`;
+            rPage.style.animation = `${behavior.fadeOutKeyframeName} ${behavior.duration}`;
+            cPage.style.animationFillMode = "forwards";
+            rPage.style.animationFillMode = "forwards";
+        } else {
+            const behavior = popBehavior;
+            cPage.style.animation = `${behavior.fadeInKeyframeName} ${behavior.duration}`;
+            rPage.style.animation = `${behavior.fadeOutKeyframeName} ${behavior.duration}`;
+            cPage.style.animationFillMode = "forwards";
+            rPage.style.animationFillMode = "forwards";
+            rPage.onanimationend = () => setPages(pages.splice(0, pages.length - 1));
+        }
+    }, [status]);
+
     return (
-        <AnimatedSize duration={behavior.duration}>
-            <Row children={pages[pages.length - 1]} />
+        <AnimatedSize duration={pushBehavior.duration}>
+            <Row position="relative" children={pages.map((page, i) => {
+                if (status == AnimatedPageStatus.none || pages.length == 1) return page;
+                if (status == AnimatedPageStatus.push) {
+                    if (i != pages.length - 1) {
+                        return <Ghost key={i} refer={rpRef} children={page} />
+                    } else {
+                        return <div key={i} ref={cpRef}>{page}</div>;
+                    }
+                } else {
+                    if (i == pages.length - 2) {
+                        return <div key={i} ref={cpRef}>{page}</div>;
+                    } else {
+                        return <Ghost key={i} refer={rpRef}>{page}</Ghost>
+                    }
+                }
+            })} />
         </AnimatedSize>
+    )
+}
+
+function Ghost({refer, children}: {
+    refer: Ref<HTMLDivElement>
+    children: ReactNode,
+}) {
+    return (
+        <Box refer={refer} position="absolute" width="0px" height="0px">
+            <Box minWidth="max-content" maxWidth="max-content" children={children} />
+        </Box>
     )
 }
