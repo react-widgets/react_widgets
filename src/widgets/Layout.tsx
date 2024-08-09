fcc052a75ac09d7a839c7e9e820d54e814b47e9f
Dev TtangKong <ttankkeo112@gmail.com>
2024-08-09 14:55:52 +0900

    Update about Layout widget for ResizeObserver

--- src/widgets/Layout.tsx
+++ src/widgets/Layout.tsx
@@ -1,33 +1,45 @@
-import { useLayoutEffect, useRef } from "react";
+import { ReactNode, useLayoutEffect, useRef } from "react";
 import { MeasuredSize } from "../types";
 
+/**
+ * Signature for the callback function of a measured size about width, height
+ * for target element about `Layout` widget.
+ */
 export type LayoutCallback = (size: MeasuredSize, element: HTMLElement) => void;
 
 export function Layout({before, behind, children}: {
     before?: LayoutCallback,
     behind?: LayoutCallback,
-    children: JSX.Element
+    children: ReactNode
 }) {
     const wrapperRef = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
         const element = wrapperRef.current as HTMLElement;
+        const onRender = () => {
+            // Removes the style properties of a wrapper for layout target element,
+            // To measure the exact size of an element.
+            element.removeAttribute("style");
 
-        // Removes the style properties of a wrapper for layout target element,
-        // To measure the exact size of an element.
-        element.removeAttribute("style");
+            if (before) {
+                const rect = element.getBoundingClientRect();
+                before(rect, element);
+            }
+            if (behind) {
+                console.assert(before != null, "[before] and [behind] must be used together.");
+                
+                const rect = element.getBoundingClientRect();
+                behind(rect, element);
+            }
+        }
 
-        if (before) {
-            const rect = element.getBoundingClientRect();
-            before(rect, element);
-        }
-        if (behind) {
-            console.assert(before != null, "[before] and [behind] must be used together.");
-            
-            const rect = element.getBoundingClientRect();
-            behind(rect, element);
-        }
-    }, [children]);
+        // Even if the component is not re-components, when the element's size changes,
+        // it must go through a separate rendering process.
+        const observer = new ResizeObserver(onRender);
+        observer.observe(element);
+
+        return () => observer.disconnect();
+    }, []);
 
     return <div ref={wrapperRef}>{children}</div>;
 }
