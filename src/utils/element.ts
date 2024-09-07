758afc6cd812292bdc0606253efee973ddf9c43a
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-07 15:01:21 +0900

    Update measureSize to support border width

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -15,10 +15,14 @@ export class ElementUtil {
         const paddingR = parseFloat(style.paddingRight);
         const paddingT = parseFloat(style.paddingTop);
         const paddingB = parseFloat(style.paddingBottom);
+        const borderL = parseFloat(style.borderLeft);
+        const borderR = parseFloat(style.borderRight);
+        const borderT = parseFloat(style.borderTop);
+        const borderB = parseFloat(style.borderBottom);
 
         return {
-            width: width + paddingL + paddingR,
-            height: height + paddingT + paddingB
+            width: width + paddingL + paddingR + borderL + borderR,
+            height: height + paddingT + paddingB + borderT + borderB
         };
     }
 }
