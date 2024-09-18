9ac16332466a82e074ec9ebc561e2d0dfbcdf00c
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-18 15:58:49 +0900

    Fixed an issue-2 about Invisible widget

--- src/widgets/Invisible.tsx
+++ src/widgets/Invisible.tsx
@@ -19,30 +19,13 @@ export function Invisible({size, width, height, children, threshold = 1e-10}: {
 
     useLayoutEffect(() => {
         const wrapper = wrapperRef.current;
-
         const observer = new IntersectionObserver(entries => {
             for (const entry of entries) {
                 // If the wrapper element is not visible, hide the child element.
-                if (entry.intersectionRatio == 0) {
-                    const _width = width ?? size;
-                    const _height = height ?? size;
-
-                    // Avoid calling the computedStyle function if a intrinsic size of
-                    // a explicitly unique child element is already provided.
-                    if (_width != null && _height != null) {
-                        wrapper.style.width = _width;
-                        wrapper.style.height = _height;
-                    } else {
-                        const intrinsicSize = ElementUtil.intrinsicSizeOf(wrapper.firstElementChild);
-                        wrapper.style.width = `${intrinsicSize.width}px`;
-                        wrapper.style.height = `${intrinsicSize.height}px`;
-                    }
-
-                    wrapper.setAttribute("active", "");
-                } else {
-                    // If the wrapper becomes visible, restore the child element display.
-                    wrapper.removeAttribute("active");
-                }
+                // And, if the wrapper becomes visible, restore the child element display.
+                entry.intersectionRatio == 0
+                    ? wrapper.setAttribute("active", "")
+                    : wrapper.removeAttribute("active");
             }
         }, {threshold});
 
@@ -50,6 +33,23 @@ export function Invisible({size, width, height, children, threshold = 1e-10}: {
 
         return () => observer.disconnect();
     }, []);
+    
+    useLayoutEffect(() => {
+        const wrapper = wrapperRef.current;
+        const _width = width ?? size;
+        const _height = height ?? size;
+
+        // Avoid calling the computedStyle function if a intrinsic size of
+        // a explicitly unique child element is already provided.
+        if (_width != null && _height != null) {
+            wrapper.style.width = _width;
+            wrapper.style.height = _height;
+        } else {
+            const intrinsicSize = ElementUtil.intrinsicSizeOf(wrapper.firstElementChild);
+            wrapper.style.width = `${intrinsicSize.width}px`;
+            wrapper.style.height = `${intrinsicSize.height}px`;
+        }
+    }, [size, width, height]);
 
     return (
         <widget-invisible ref={wrapperRef} children={children} />
